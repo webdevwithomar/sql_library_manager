2a1fcb0594aa14468c46be44731ffc6934ea35cb
Omar Faruque <omar.faruque967@gmail.com>
2019-01-02 03:24:16 +0600

    add one route and create some pug files

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
-
+const path = require('path');
 
 // Init app
 const app = express();
+
+// Load View Engine
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'pug');
+
+// Using static assets
+app.use('/public', express.static('public'));
+
+// Routes
+const mainRoute = require('./routes');
+
+app.use(mainRoute);
 
 app.listen(3000, () => {
   console.log('Listening at port 3000');
