8ddaa43b247c8ea8f5d7f1f725b8a3184ee027a9
Omar Faruque <omar.faruque967@gmail.com>
2019-01-02 20:23:26 +0600

    fix some changes and change testing and production to library.db

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/public', express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, public)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 const mainRoute = require('./routes');
--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routes
 router.get('/books', (req, res, next) => {
-  Books.findAll().then((books) => {
-    res.render('index', { title: title, author: author, genre: genre, year: year });
+  Books.findAll({
+    order: [['id', 'ASC']]
   })
+    .then(books => {
+      const booksPerPage = 10;
+      const numberOfPages = Math.ceil(books.length / booksPerPage);
+      const activePage = req.query.page;
+      let totalBooks = [];
+
+      while (books.length > 0) {
+        totalBooks.push(books.splice(0, booksPerPage));
+      }
+
+      if (typeof currentPage === 'undefined') {
+        currentPage = 0;
+      }
+
+      res.render('book-list', {
+        books: totalBooks[activePage],
+        pages: numberOfPages
+      });
+    });
 });
 
+// Redirecting the root route to books
 router.get('/', (req, res) => {
   res.redirect('/books');
 });
 
-router.get('/books/new', (req, res, next) => {
-  res.render('new_book');
-});
+// Posting new book
+
+
+// router.get('/books/new', (req, res, next) => {
+//   res.render('new_book');
+// });
 
 // Getting indiviudal books
 router.get('/books/:id', (req, res, next) => {
