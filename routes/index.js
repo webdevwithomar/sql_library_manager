0ff4ccb2ebf5f49c7a6255b6357c54e523ee0abd
Omar Faruque <omar.faruque967@gmail.com>
2019-01-02 20:48:19 +0600

    fix index file

--- routes/index.js
+++ routes/index.js
@@ -11,16 +11,8 @@ router.get('/', (req, res) => {
   res.redirect('/books');
 });
 
-
-// router.get('/books/new', (req, res, next) => {
-//   res.render('new_book');
-// });
-
-// Getting indiviudal books
-router.get('/books/:id', (req, res, next) => {
-  Books.findById(req.params.id).then((book) => {
-    res.render('/books/show', { title: title, author: author, genre: genre, year: year });
-  });
+router.get('/books/new', (req, res, next) => {
+  res.render('new_book', { book: Book.build() });
 });
 
 module.exports = router;
