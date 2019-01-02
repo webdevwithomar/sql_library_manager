974e3e5964c394f4f6f9d808baa904eab1a0c2c7
Omar Faruque <omar.faruque967@gmail.com>
2019-01-02 12:31:38 +0600

    init sequelize and add routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 // Routes
-router.get('/', (req, res) => {
+router.get('/books', (req, res) => {
   res.render('index');
+});
+
+router.get('/books/:id', (req, res) => {
+  res.render('project', {
+
+  });
+});
+
+router.get('/', (req, res) => {
+  res.redirect('/books');
+});
+
+router.get('/books/new', (req, res) => {
+  res.render('new_book');
 });
 
 module.exports = router;
